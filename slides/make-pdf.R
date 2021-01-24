6329f5260e14117531b0cacf79601278508de98f
Ivan Rudik <irudik@cornell.edu>
2021-01-23 19:45:19 -0500

    Add pdf versions of files.

---
+++ slides/make-pdf.R
@@ -0,0 +1,10 @@
+# Makes pdf slides for all lectures
+
+lapply(list.files(pattern = "*.html"), 
+       function(file) {
+         xaringan::decktape(
+           file, 
+           output = paste0(tools::file_path_sans_ext(file), ".pdf")
+           )
+       }
+)
