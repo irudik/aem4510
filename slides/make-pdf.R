d184330239f389ce0ac234ff587f86f66ed59559
Ivan Rudik <irudik@cornell.edu>
2021-02-23 12:06:13 -0500

    Fix error in ln4.

--- slides/make-pdf.R
+++ slides/make-pdf.R
@@ -1,6 +1,6 @@
 # Makes pdf slides for all lectures
 
-lapply(list.files(pattern = "*.html", recursive = T)[3:4], 
+lapply(list.files(pattern = "*.html", recursive = T)[4], 
        function(file) {
          xaringan::decktape(
            file, 
