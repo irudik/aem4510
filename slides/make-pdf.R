2af8d17cd5ecbc63ad19f1b4b8f93303ff2e584e
Ivan Rudik <irudik@cornell.edu>
2021-03-15 12:42:44 -0400

    update second-best slides.

--- slides/make-pdf.R
+++ slides/make-pdf.R
@@ -1,5 +1,5 @@
 # Makes pdf slides for all lectures
-lapply(list.files(pattern = "*.html", recursive = T)[6:7], 
+lapply(list.files(pattern = "*.html", recursive = T)[7], 
        function(file) {
          xaringan::decktape(
            file, 
