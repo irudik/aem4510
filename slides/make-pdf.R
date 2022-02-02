0bfba88103ec9008fc181077a9178106ce04311f
Ivan Rudik <irudik@cornell.edu>
2022-02-01 20:54:58 -0500

    new ln3

--- slides/make-pdf.R
+++ slides/make-pdf.R
@@ -1,5 +1,5 @@
 # Makes pdf slides for all lectures
-lapply(list.files(pattern = "*.html", recursive = T)[2], 
+lapply(list.files(pattern = "*.html", recursive = T)[3], 
        function(file) {
          xaringan::decktape(
            file, 
