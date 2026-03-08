362561d479982b9c5624e0fbc55a3651326af1a1
Ivan <irudik@cornell.edu>
2026-03-08 09:22:55 -0400

    Point PDF script to taxes deck output

--- slides/make-pdf.R
+++ slides/make-pdf.R
@@ -1,5 +1,5 @@
 # Makes pdf slides for all lectures
-lapply(list.files(path = "slides", pattern = "\\.html$", full.names = TRUE)[3],
+lapply(list.files(path = "slides", pattern = "\\.html$", full.names = TRUE)[5],
        function(file) {
          xaringan::decktape(
            file,
